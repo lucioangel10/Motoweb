feccf0c20a0c13475822c1da344f19f8460d0094
lucioangel10 <96026330+lucioangel10@users.noreply.github.com>
2022-02-04 15:08:56 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,88 +1,107 @@
-const tazasInteres= [1.25, 1.35, 1.45]
-const motos=[]
 
 
 // cotizador de cuotas
-class Simulador{
-    constructor(precio, anticipo){
-        this.precio= parseFloat(precio);
-        this.anticipo= parseFloat(anticipo);   
+class Simulador {
+    constructor(precio, anticipo) {
+        this.precio=precio;
+        this.anticipo = parseFloat(anticipo);
         this.resultado1;
         this.resultado2;
         this.resultado3;
     }
-    cuotas12(){
-        this.resultado1= this.precio-this.anticipo;
-        this.resultado1= this.resultado1 * tazasInteres[0];
-        this.resultado1= this.resultado1/12;
+    cuotas12() {
+        this.resultado1 = this.precio - this.anticipo;
+        this.resultado1 = this.resultado1 * tazasInteres[0];
+        this.resultado1 = this.resultado1 / 12;
     }
-    cuotas18(){
-        this.resultado2= this.precio-this.anticipo;
-        this.resultado2= this.resultado2 * tazasInteres[1];
-        this.resultado2= this.resultado2/18;
+    cuotas18() {
+        this.resultado2 = this.precio - this.anticipo;
+        this.resultado2 = this.resultado2 * tazasInteres[1];
+        this.resultado2 = this.resultado2 / 18;
     }
-    cuotas24(){
-        this.resultado3= this.precio-this.anticipo;
-        this.resultado3= this.resultado3 * tazasInteres[2];
-        this.resultado3= this.resultado3/24;
+    cuotas24() {
+        this.resultado3 = this.precio - this.anticipo;
+        this.resultado3 = this.resultado3 * tazasInteres[2];
+        this.resultado3 = this.resultado3 / 24;
     }
-    mostrar(){
+    mostrar() {
         this.cuotas12();
         this.cuotas18();
         this.cuotas24();
         alert(`La financiacion para el importe y anticipo elejido es:\n12 cuotas de $${this.resultado1}\n18 cuotas de $${this.resultado2}\n24 cuotas de $${this.resultado3}`);
     }
 }
-class Moto{
-    constructor(nombre, precio, tipo, marca){
-        this.nombre= nombre.toUpperCase();
-        this.precio= parseFloat(precio);
-        this.tipo= tipo.toUpperCase();
-        this.marca=marca.toUpperCase();
+class Moto {
+    constructor(nombre, precio, tipo, marca) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.tipo = tipo.toUpperCase();
+        this.marca = marca.toUpperCase();
 
     }
+
+}
+function pedirMoto() {
+    ingreso = prompt('Ingrese el modelo que desea cotizar:');
+    ingreso = ingreso.toUpperCase();
+}
+function pedirAnticipo() {
+
+    anticipo = parseFloat(prompt('Si desea entregar un anticipo ingreseló, sino precione aceptar'));
+}
+function corroborarAnticipo(anticipo, precio){
+    if(anticipo>precio || isNaN(anticipo) || anticipo<0){
+        alert("el dato ingresado no es valido")
+        return false;
+
+    }
+    return true;
+}
+
+
+
+
+const tazasInteres = [1.25, 1.35, 1.45]
+const motos = []
+
+motos.push(new Moto('Tornado', '880000', 'onoff', 'Honda'));
+motos.push(new Moto('Titan', '460000', 'calle', 'honda'));
+
+motos.push(new Moto('wave', '220000', 'cub', 'honda'));
+
+motos.push(new Moto('Fascino', '472600', 'scooter', 'yamaha'));
+
+motos.push(new Moto('fzfi d', '542600', 'calle', 'yamaha'));
+motos.push(new Moto('xtz 250', '997000', 'onoff', 'yamaha'));
+
+let ingreso;
+let anticipo;
+
+
+const arrayNombre=motos.map((el)=>el.nombre);
+
+
+
+
+
+
+
+
+for (let i = 1; i > 0; i++) {
+
+    pedirMoto();
+    pedirAnticipo();
+
+    let verificarMoto = motos.some((el) => el.nombre == ingreso);
+    if (verificarMoto == true && corroborarAnticipo(anticipo, ingreso)) {
+        let seleccion=motos.find((el)=>el.nombre.includes(ingreso));
+        let simulador= new Simulador(seleccion.precio, anticipo);
+        simulador.mostrar();
     
+    } else {
+        alert('Los valores ingresados no se han encontrado o no son validos.\nModelos válidos:\n'+arrayNombre.join(", "))
+    }
 }
-motos.push(new Moto('Tornado', '880000', 'onoff','Honda'));
-motos.push(new Moto('Cg titan', '460000', 'calle','honda'));
+        
+    
 
-motos.push(new Moto('wave 110', '220000', 'cub','honda'));
-
-motos.push(new Moto('fascino 125', '472600', 'scooter','yamaha'));
-
-motos.push(new Moto('fz-s fi d', '542600', 'calle','yamaha'));
-motos.push(new Moto('xtz 250 abs', '997000', 'onoff','yamaha'));
-
-
-
-function pedirDatos() {
-     precio=prompt('Ingrese valor de la moto deseada:');
-     anticipo=prompt('Si desea entregar un anticipo ingreseló, sino ingrese el numero 0');
-}
-function corroborarDatos(precio, anticipo) {
-    let resultado1=isNaN(precio);
-    let resultado2=isNaN(anticipo);
-    if(resultado1==false && resultado2==false && resultado1!=null && precio>anticipo && resultado1>0 && resultado2>=0){
-        return true;
-    }else{
-    return false;
-}
-}
-
-
-
-// let precio;
-// let anticipo;
-
-// for(let i=1; i>0;i++){
-
-// pedirDatos();
-
-// let corroborar=corroborarDatos(precio, anticipo);
-// if(corroborar==true){
-//     let simulador= new Simulador(precio, anticipo);
-//     simulador.mostrar();
-// }else if(corroborar==false){
-//     alert('Los valores ingresados no son correctos.\nPresiona "Aceptar" para volver a simular.')
-// }
-// }
